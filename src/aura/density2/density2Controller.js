186ff96edf7b80a67e662249cc7bfc53f71cda0d
patrick-fischer <patrick-fischer@users.noreply.github.com>
2019-01-21 09:23:52 +0000

    Initial Commit of Density Package
    Includes 4 Aura Components, VF Page, Apex Controller:
    * aura/density[1-4].cmp
    * pages/MySessionIdVFPage.page
    * classes/DensityAuraController.cls

---
+++ src/aura/density2/density2Controller.js
@@ -0,0 +1,5 @@
+({
+    doInit : function(cmp, event, helper) {
+        helper.initialiseDensity(cmp, event, helper);
+    }
+})
---
+++ src/aura/density4/density4Helper.js
@@ -0,0 +1,13 @@
+({
+    initialiseDensity : function(cmp, event, helper) {
+        let action = cmp.get('c.getDensityFromUIApi');
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                console.log('Response: ' + response.getReturnValue());
+                cmp.set('v.density', response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    }
+});
